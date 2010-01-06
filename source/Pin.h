2317b08c21c1914dfaacaed7455c7f640e8fedd5
Tobias Weyand <me@tobw.net>
2010-01-06 15:27:41 +0100

    GPL'd

--- source/Pin.h
+++ source/Pin.h
@@ -1,3 +1,23 @@
+/*
+ *    Pocket Physics - A mechanical construction kit for Nintendo DS
+ *                   Copyright 2005-2010 Tobias Weyand (me@tobw.net)
+ *                            http://code.google.com/p/pocketphysics
+ *
+ * TobKit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * TobKit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with Pocket Physics. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef PIN_H_
 #define PIN_H_
 
